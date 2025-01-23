8a7f0623337124b4082c3feb73356c269a4dd525
Rohini98045 <rohiniagarwal2901@gmail.com>
2025-01-23 15:47:03 +0530

    Update Test.java
    add m2

--- GitHubDemo/src/com/main/Test.java
+++ GitHubDemo/src/com/main/Test.java
@@ -5,6 +5,9 @@ public int m1(int a,int b) {
 int c=a+b;
 return c;
 }
+  public void m2(){
+    System.out.println("Hii");
+  }
 public static void main(String[] args) {
 Test t=new Test();
 int c=t.m1(10, 20);
