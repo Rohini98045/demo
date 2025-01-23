f3ad6f3dd136d1c29c463fb3bff92ad7f5b8fe37
Rohini98045 <rohiniagarwal2901@gmail.com>
2025-01-23 15:08:04 +0530

    Update Test.java
    add m2

--- GitDemo/src/com/main/Test.java
+++ GitDemo/src/com/main/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public void m1() {
 		System.out.println("Hello world");
 	}
+	public void m2(){
+	System.out.println("Hello world M2");
+	}
 public static void main(String[] args) {
 	Test t=new Test();
 	t.m1();
