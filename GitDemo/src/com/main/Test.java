8a9c2afb8b0a07a011546defb6379d8ece30409d
ProgramWind <121439140+ProgramWind@users.noreply.github.com>
2025-01-23 15:04:03 +0530

    add m1 method

---
+++ GitDemo/src/com/main/Test.java
@@ -0,0 +1,11 @@
+package com.main;
+
+public class Test {
+	public void m1() {
+		System.out.println("Hello world");
+	}
+public static void main(String[] args) {
+	Test t=new Test();
+	t.m1();
+}
+}
